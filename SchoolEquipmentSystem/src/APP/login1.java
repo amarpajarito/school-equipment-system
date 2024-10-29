d8881139834401ecafdb2e466018c313dd44e616
Railey <Railey@Railey>
2024-10-29 13:57:20 +0800

    new login

--- SchoolEquipmentSystem/src/APP/login1.java
+++ SchoolEquipmentSystem/src/APP/login1.java
@@ -10,12 +10,12 @@ import javax.swing.JOptionPane;
  *
  * @author Railey
  */
-public class login extends javax.swing.JFrame {
+public class login1 extends javax.swing.JFrame {
 
     /**
      * Creates new form loginDetails
      */
-    public login() {
+    public login1() {
         initComponents();
     }
 
@@ -253,21 +253,23 @@ public class login extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(login1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(login1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(login1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(login1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new login().setVisible(true);
+                new login1().setVisible(true);
             }
         });
     }
