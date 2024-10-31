a4f74bf6b636746300b144c28fb8d3b336081632
amarpajarito <amarpajarito04@gmail.com>
2024-11-01 01:03:08 +0800

    New Form and Fixed Backend Libraries

--- SchoolEquipmentSystem/src/APP/Menu.java
+++ SchoolEquipmentSystem/src/APP/Menu.java
@@ -142,8 +142,6 @@ public class Menu extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
-        this.dispose();
-        new AddEquipment().setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
