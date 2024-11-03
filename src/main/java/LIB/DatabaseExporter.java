12011e6eacb496109cddfe78fd124393987a1ab2
amarpajarito <amarpajarito04@gmail.com>
2024-11-03 08:37:45 +0800

    Added option to choose desired saved file location

--- src/main/java/LIB/DatabaseExporter.java
+++ src/main/java/LIB/DatabaseExporter.java
@@ -4,6 +4,7 @@
  */
 package LIB;
 
+import java.io.File;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.FileOutputStream;
@@ -15,151 +16,183 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import javax.swing.JFileChooser;
 /**
  *
  * @author Amar Pajarito
  */
 public class DatabaseExporter {
-    private final String DB_URL = "jdbc:sqlite:SchoolEquipmentSystem.db";
-    private static final String CSV_FILE_PATH = "C:/Users/Amar Pajarito/Desktop/College Files/2ND_TERM1/CSS123L/school-equipment-system/SchoolEquipmentSystem/src/CSV/equipment_export.csv";
 
     public void exportAllEquipmentsToCSV() {
-        try (Connection conn = DriverManager.getConnection(DB_URL);
-             Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery("SELECT * FROM EQUIPMENT");
-             FileWriter csvWriter = new FileWriter(CSV_FILE_PATH)) {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Specify a file to save");
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        fileChooser.setSelectedFile(new File("all_equipment_report.csv"));
+        int userSelection = fileChooser.showSaveDialog(null);
 
-            ResultSetMetaData rsMetaData = rs.getMetaData();
-            int columnCount = rsMetaData.getColumnCount();
+        if (userSelection == JFileChooser.APPROVE_OPTION) {
+            File fileToSave = fileChooser.getSelectedFile();
+            try (Connection conn = DatabaseConnection.getInstance().getConnection();
+                 Statement stmt = conn.createStatement();
+                 ResultSet rs = stmt.executeQuery("SELECT * FROM EQUIPMENT");
+                 FileWriter csvWriter = new FileWriter(fileToSave)) {
 
-            for (int i = 1; i <= columnCount; i++) {
-                csvWriter.append(rsMetaData.getColumnName(i));
-                if (i < columnCount) csvWriter.append(",");
-            }
-            csvWriter.append("\n");
+                ResultSetMetaData rsMetaData = rs.getMetaData();
+                int columnCount = rsMetaData.getColumnCount();
 
-            while (rs.next()) {
                 for (int i = 1; i <= columnCount; i++) {
-                    csvWriter.append(rs.getString(i));
+                    csvWriter.append(rsMetaData.getColumnName(i));
                     if (i < columnCount) csvWriter.append(",");
                 }
                 csvWriter.append("\n");
-            }
 
-            System.out.println("All equipments successfully exported to " + CSV_FILE_PATH);
-
-        } catch (IOException | SQLException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    public void exportReplaceLostEquipmentstToCSV() {
-        String query = "SELECT * FROM EQUIPMENT WHERE condition IN ('For Replacement', 'Lost')";
-        String csvFilePath = "C:/Users/Amar Pajarito/Desktop/College Files/2ND_TERM1/CSS123L/school-equipment-system/SchoolEquipmentSystem/src/CSV/damaged_equipment_export.csv"; // Change the path if needed
-
-        try (Connection conn = DriverManager.getConnection(DB_URL);
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-            FileWriter csvWriter = new FileWriter(csvFilePath)) {
-
-            ResultSetMetaData rsMetaData = rs.getMetaData();
-            int columnCount = rsMetaData.getColumnCount();
-
-            for (int i = 1; i <= columnCount; i++) {
-                csvWriter.append(rsMetaData.getColumnName(i));
-                if (i < columnCount) csvWriter.append(",");
-            }
-            csvWriter.append("\n");
-
-            while (rs.next()) {
-                for (int i = 1; i <= columnCount; i++) {
-                    csvWriter.append(rs.getString(i));
-                    if (i < columnCount) csvWriter.append(",");
+                while (rs.next()) {
+                    for (int i = 1; i <= columnCount; i++) {
+                        csvWriter.append(rs.getString(i));
+                        if (i < columnCount) csvWriter.append(",");
+                    }
+                    csvWriter.append("\n");
                 }
-                csvWriter.append("\n");
+
+                System.out.println("All equipments successfully exported to " + fileToSave.getAbsolutePath());
+
+            } catch (IOException | SQLException e) {
+                e.printStackTrace();
             }
-
-            System.out.println("For Replacement and Lost equipment data successfully exported to " + csvFilePath);
-
-        } catch (IOException | SQLException e) {
-            e.printStackTrace();
         }
     }
-    
+
     public void exportAllEquipmentsToExcel() {
-        String excelFilePath = "C:/Users/Amar Pajarito/Desktop/College Files/2ND_TERM1/CSS123L/school-equipment-system/SchoolEquipmentSystem/src/CSV/equipment_export.xlsx";
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Specify a file to save");
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        fileChooser.setSelectedFile(new File("all_equipment_report.xlsx"));
+        int userSelection = fileChooser.showSaveDialog(null);
 
-        try (Connection conn = DriverManager.getConnection(DB_URL);
-             Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery("SELECT * FROM EQUIPMENT");
-             Workbook workbook = new XSSFWorkbook()) {
+        if (userSelection == JFileChooser.APPROVE_OPTION) {
+            File fileToSave = fileChooser.getSelectedFile();
+            try (Connection conn = DatabaseConnection.getInstance().getConnection();
+                 Statement stmt = conn.createStatement();
+                 ResultSet rs = stmt.executeQuery("SELECT * FROM EQUIPMENT");
+                 Workbook workbook = new XSSFWorkbook()) {
 
-            Sheet sheet = workbook.createSheet("All Equipment");
-            ResultSetMetaData rsMetaData = rs.getMetaData();
-            int columnCount = rsMetaData.getColumnCount();
+                Sheet sheet = workbook.createSheet("All Equipment");
+                ResultSetMetaData rsMetaData = rs.getMetaData();
+                int columnCount = rsMetaData.getColumnCount();
 
-            Row headerRow = sheet.createRow(0);
-            for (int i = 1; i <= columnCount; i++) {
-                Cell cell = headerRow.createCell(i - 1);
-                cell.setCellValue(rsMetaData.getColumnName(i));
-            }
-
-            int rowCount = 1;
-            while (rs.next()) {
-                Row dataRow = sheet.createRow(rowCount++);
+                Row headerRow = sheet.createRow(0);
                 for (int i = 1; i <= columnCount; i++) {
-                    Cell cell = dataRow.createCell(i - 1);
-                    cell.setCellValue(rs.getString(i));
+                    Cell cell = headerRow.createCell(i - 1);
+                    cell.setCellValue(rsMetaData.getColumnName(i));
                 }
+
+                int rowCount = 1;
+                while (rs.next()) {
+                    Row dataRow = sheet.createRow(rowCount++);
+                    for (int i = 1; i <= columnCount; i++) {
+                        Cell cell = dataRow.createCell(i - 1);
+                        cell.setCellValue(rs.getString(i));
+                    }
+                }
+
+                try (FileOutputStream outputStream = new FileOutputStream(fileToSave)) {
+                    workbook.write(outputStream);
+                }
+
+                System.out.println("All equipments successfully exported to " + fileToSave.getAbsolutePath());
+
+            } catch (IOException | SQLException e) {
+                e.printStackTrace();
             }
+        }
+    }
 
-            try (FileOutputStream outputStream = new FileOutputStream(excelFilePath)) {
-                workbook.write(outputStream);
+    public void exportReplaceLostEquipmentsToCSV() {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Specify a file to save");
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        fileChooser.setSelectedFile(new File("replace_lost_equipment_report.csv"));
+        int userSelection = fileChooser.showSaveDialog(null);
+
+        if (userSelection == JFileChooser.APPROVE_OPTION) {
+            File fileToSave = fileChooser.getSelectedFile();
+            String query = "SELECT * FROM EQUIPMENT WHERE condition IN ('For Replacement', 'Lost')";
+
+            try (Connection conn = DatabaseConnection.getInstance().getConnection();
+                 Statement stmt = conn.createStatement();
+                 ResultSet rs = stmt.executeQuery(query);
+                 FileWriter csvWriter = new FileWriter(fileToSave)) {
+
+                ResultSetMetaData rsMetaData = rs.getMetaData();
+                int columnCount = rsMetaData.getColumnCount();
+
+                for (int i = 1; i <= columnCount; i++) {
+                    csvWriter.append(rsMetaData.getColumnName(i));
+                    if (i < columnCount) csvWriter.append(",");
+                }
+                csvWriter.append("\n");
+
+                while (rs.next()) {
+                    for (int i = 1; i <= columnCount; i++) {
+                        csvWriter.append(rs.getString(i));
+                        if (i < columnCount) csvWriter.append(",");
+                    }
+                    csvWriter.append("\n");
+                }
+
+                System.out.println("For Replacement and Lost equipment data successfully exported to " + fileToSave.getAbsolutePath());
+
+            } catch (IOException | SQLException e) {
+                e.printStackTrace();
             }
-
-            System.out.println("All equipments successfully exported to " + excelFilePath);
-
-        } catch (IOException | SQLException e) {
-            e.printStackTrace();
         }
     }
 
     public void exportReplaceLostEquipmentsToExcel() {
-        String query = "SELECT * FROM EQUIPMENT WHERE condition IN ('For Replacement', 'Lost')";
-        String excelFilePath = "C:/Users/Amar Pajarito/Desktop/College Files/2ND_TERM1/CSS123L/school-equipment-system/SchoolEquipmentSystem/src/CSV/damaged_equipment_export.xlsx"; // Change the path if needed
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Specify a file to save");
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        fileChooser.setSelectedFile(new File("replace_lost_equipment_report.xlsx"));
+        int userSelection = fileChooser.showSaveDialog(null);
 
-        try (Connection conn = DriverManager.getConnection(DB_URL);
-             Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery(query);
-             Workbook workbook = new XSSFWorkbook()) {
+        if (userSelection == JFileChooser.APPROVE_OPTION) {
+            File fileToSave = fileChooser.getSelectedFile();
+            String query = "SELECT * FROM EQUIPMENT WHERE condition IN ('For Replacement', 'Lost')";
 
-            Sheet sheet = workbook.createSheet("Damaged Equipment");
-            ResultSetMetaData rsMetaData = rs.getMetaData();
-            int columnCount = rsMetaData.getColumnCount();
+            try (Connection conn = DatabaseConnection.getInstance().getConnection();
+                 Statement stmt = conn.createStatement();
+                 ResultSet rs = stmt.executeQuery(query);
+                 Workbook workbook = new XSSFWorkbook()) {
 
-            Row headerRow = sheet.createRow(0);
-            for (int i = 1; i <= columnCount; i++) {
-                Cell cell = headerRow.createCell(i - 1);
-                cell.setCellValue(rsMetaData.getColumnName(i));
-            }
+                Sheet sheet = workbook.createSheet("Damaged Equipment");
+                ResultSetMetaData rsMetaData = rs.getMetaData();
+                int columnCount = rsMetaData.getColumnCount();
 
-            int rowCount = 1;
-            while (rs.next()) {
-                Row dataRow = sheet.createRow(rowCount++);
+                Row headerRow = sheet.createRow(0);
                 for (int i = 1; i <= columnCount; i++) {
-                    Cell cell = dataRow.createCell(i - 1);
-                    cell.setCellValue(rs.getString(i));
+                    Cell cell = headerRow.createCell(i - 1);
+                    cell.setCellValue(rsMetaData.getColumnName(i));
                 }
+
+                int rowCount = 1;
+                while (rs.next()) {
+                    Row dataRow = sheet.createRow(rowCount++);
+                    for (int i = 1; i <= columnCount; i++) {
+                        Cell cell = dataRow.createCell(i - 1);
+                        cell.setCellValue(rs.getString(i));
+                    }
+                }
+
+                try (FileOutputStream outputStream = new FileOutputStream(fileToSave)) {
+                    workbook.write(outputStream);
+                }
+
+                System.out.println("For Replacement and Lost equipment data successfully exported to " + fileToSave.getAbsolutePath());
+
+            } catch (IOException | SQLException e) {
+                e.printStackTrace();
             }
-
-            try (FileOutputStream outputStream = new FileOutputStream(excelFilePath)) {
-                workbook.write(outputStream);
-            }
-
-            System.out.println("For Replacement and Lost equipment data successfully exported to " + excelFilePath);
-
-        } catch (IOException | SQLException e) {
-            e.printStackTrace();
         }
     }
 }
+
